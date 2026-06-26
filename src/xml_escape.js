db39fea797d2c7248f98d2d5f5ea0cb57ffb4f4b
Gnossan <163731356+Gnossan@users.noreply.github.com>
2026-06-26 13:41:42 +0200

    Fas 1, Issue 4: Skapa src/xml_escape.js med xmlEscape-funktion

---
+++ src/xml_escape.js
@@ -0,0 +1,8 @@
+module.exports.xmlEscape = function xmlEscape(text) {
+  return String(text)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+};
