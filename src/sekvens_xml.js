4e884d2892d0ff07979aa58eb3dc1949a21d5065
Gnossan <163731356+Gnossan@users.noreply.github.com>
2026-06-26 13:48:02 +0200

    Fas 1, Issue 5: Skapa sekvens_xml.js

---
+++ src/sekvens_xml.js
@@ -0,0 +1,37 @@
+function sekvensTillDrawio(kalla) {
+  var data = window.sekvensParser(kalla);
+  var noder = [];
+  var kanter = [];
+  var idCounter = 1;
+
+  for (var i = 0; i < data.deltagare.length; i++) {
+    noder.push({
+      id: String(idCounter++),
+      etikett: data.deltagare[i],
+      x: i * 200,
+      y: 20,
+      bredd: 120,
+      hojd: 40,
+      stil: "whiteSpace=wrap;html=1;perimeter=rectanglePerimeter"
+    });
+  }
+
+  for (var j = 0; j < data.meddelanden.length; j++) {
+    var m = data.meddelanden[j];
+    var kallaIndex = data.deltagare.indexOf(m.kalla);
+    var malIndex = data.deltagare.indexOf(m.mal);
+
+    if (kallaIndex >= 0 && malIndex >= 0) {
+      kanter.push({
+        id: String(idCounter++),
+        kallaId: String(kallaIndex + 1),
+        malId: String(malIndex + 1),
+        etikett: m.etikett
+      });
+    }
+  }
+
+  return window.skapaMxGraphDokument(noder, kanter);
+}
+
+window.sekvensTillDrawio = sekvensTillDrawio;
