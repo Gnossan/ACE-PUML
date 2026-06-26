454a3a3cb9a52f71ca5fd185d8d053194680d7df
Gnossan <163731356+Gnossan@users.noreply.github.com>
2026-06-26 13:48:03 +0200

    Fas 1, Issue 5: Lägg till exporteraDrawio i fil_io.js

--- src/fil_io.js
+++ src/fil_io.js
@@ -1,5 +1,6 @@
 var SVG_FILTER = { name: 'SVG', extensions: ['svg'] };
 var PNG_FILTER = { name: 'PNG', extensions: ['png'] };
+var DRAWIO_FILTER = { name: 'draw.io', extensions: ['drawio'] };
 
 window.exporteraSVG = function exporteraSVG(svgInnehall) {
   return window.aceAPI.sparaFil(svgInnehall, 'diagram.svg', [SVG_FILTER]);
@@ -7,4 +8,8 @@ window.exporteraSVG = function exporteraSVG(svgInnehall) {
 
 window.exporteraPNG = function exporteraPNG(pngDataUrl) {
   return window.aceAPI.sparaFil(pngDataUrl, 'diagram.png', [PNG_FILTER]);
+};
+
+window.exporteraDrawio = function exporteraDrawio(drawioXml) {
+  return window.aceAPI.sparaFil(drawioXml, 'diagram.drawio', [DRAWIO_FILTER]);
 };
