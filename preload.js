4f85c970fbd5369ddb8d009b1a130bd994cf6321
Gnossan <163731356+Gnossan@users.noreply.github.com>
2026-06-26 13:43:40 +0200

    Fix #4: Add renderPumlPng to aceAPI in preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('aceAPI',{
   renderPuml:(code)=>ipcRenderer.invoke('render-puml',code),
+  renderPumlPng:(code)=>ipcRenderer.invoke('render-puml-png',code),
   sparaFil:(innehall,foreslagetNamn,filter)=>ipcRenderer.invoke('visa-spara-dialog',innehall,foreslagetNamn,filter)
 });
